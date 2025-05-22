5f190ac14445f815a0f67e195fc206151c0e651c
F4ntasyc <fthanakbar@gmail.com>
2025-05-22 20:12:52 +0700

    Add files via upload
    yang ini update udah ada search sm sorting

--- tubes.go
+++ tubes.go
@@ -1,17 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+	"os"
+)
 
 type pengguna struct {
 	nama, password string
 	totalPinjaman  float64
-	tenor, umur    int
+	tenor, id      int
 	status         bool
 }
 
 type admin struct {
 	nama, password string
-	Nomor          int
+	id             int
 }
 
 const max = 100
@@ -27,9 +31,32 @@ func menu1() {
 	var pilihan, banyakPengguna, banyakAdmin int
 	var p data
 	var ad adm
+	ad[0] = admin{
+		nama:     "1",
+		password: "123",
+		id:       1,
+	}
+	p[0] = pengguna{
+		nama:          "akmal",
+		password:      "1",
+		totalPinjaman: 500000,
+		tenor:         6,
+		id:            1,
+		status:        true,
+	}
+	p[1] = pengguna{
+		nama:          "fathan",
+		password:      "1",
+		totalPinjaman: 100000,
+		tenor:         12,
+		id:            2,
+		status:        false,
+	}
+	banyakPengguna += 2
+	banyakAdmin++
 	pilihan = -1
 	for pilihan != 0 {
-		fmt.Println("Menu 1")
+		fmt.Println("=== Menu ===")
 		fmt.Println("1. Login")
 		fmt.Println("2. Register")
 		fmt.Println("0. Keluar")
@@ -51,26 +78,29 @@ func menu1() {
 }
 
 func register(p *data, ad *adm, bPengguna, bAdmin *int) {
-	var adm int
-	fmt.Println("Menu Register")
+	clear()
+	var adm, id int
+	fmt.Println("=== Menu Register ===")
 	fmt.Println("1. Admin")
 	fmt.Println("2. Pengguna")
 	fmt.Print("Pilihan : ")
 	fmt.Scan(&adm)
 	if adm == 1 {
 		fmt.Println("Nama :")
-		fmt.Scan(&(*ad)[*bAdmin].nama)
+		fmt.Scan(&ad[*bAdmin].nama)
 		fmt.Println("Password : ")
-		fmt.Scan(&(*ad)[*bAdmin].password)
-		fmt.Println("Nomor HP : ")
-		fmt.Scan(&(*ad)[*bAdmin].Nomor)
+		fmt.Scan(&ad[*bAdmin].password)
+		id++
+		ad[*bAdmin].id = id
 		*bAdmin += 1
+
 	} else if adm == 2 {
 		fmt.Println("Nama : ")
 		fmt.Scan(&(*p)[*bPengguna].nama)
 		fmt.Println("Password : ")
 		fmt.Scan(&(*p)[*bPengguna].password)
-
+		id++
+		(*p)[*bPengguna].id = id
 		*bPengguna += 1
 
 	} else {
@@ -80,6 +110,7 @@ func register(p *data, ad *adm, bPengguna, bAdmin *int) {
 }
 
 func login(p *data, ad *adm, bPengguna, bAdmin *int) {
+	clear()
 	var idx, i int
 	var nama, password string
 	var user, pinjol bool
@@ -113,7 +144,7 @@ func login(p *data, ad *adm, bPengguna, bAdmin *int) {
 
 			if user {
 				if password == (*ad)[idx].password {
-					menuAdm(&*p, idx, &(*bPengguna), &pinjol)
+					menuAdm(&*p, &*ad, idx, &(*bPengguna), &pinjol)
 				} else {
 					fmt.Println("Password salah.")
 				}
@@ -155,6 +186,7 @@ func login(p *data, ad *adm, bPengguna, bAdmin *int) {
 }
 
 func menuUser(p *data, idx int, bPengguna *int, pinjol *bool) {
+	clear()
 	var pilihan int
 	var bayar float64
 	pilihan = -1
@@ -183,10 +215,11 @@ func menuUser(p *data, idx int, bPengguna *int, pinjol *bool) {
 }
 
 func painjol(p *data, pinjol *bool, bPengguna, idx *int, bayar *float64) {
+	clear()
 	//	var duid, bunga float64
 	var pilih, tenor int
 	var pinjem bool
-	fmt.Println("Halo ", (*p)[*idx].nama)
+	fmt.Println("Halo ", p[*idx].nama)
 	fmt.Println("Butuh Pinjaman Berapa")
 	fmt.Println("1. RP.500.000")
 	fmt.Println("2. RP.1.000.000")
@@ -236,13 +269,14 @@ func painjol(p *data, pinjol *bool, bPengguna, idx *int, bayar *float64) {
 				*pinjol = true
 			}
 		}
-
+		fmt.Printf("%s, Anda telah meminjam sebesar %v dengan tenor selama %d bulan\n", p[*idx].nama, (*p)[*idx].totalPinjaman, (*p)[*idx].tenor)
 		pinjem = false
 
 	}
 }
 
 func ubahUser(p *data, idx int) {
+	clear()
 	var pilih int
 	pilih = -1
 	for pilih != 0 {
@@ -266,6 +300,7 @@ func ubahUser(p *data, idx int) {
 }
 
 func membayar(p *data, idx int, bayar *float64) {
+	clear()
 	var pilih, bulan, tenor int
 
 	tenor = (*p)[idx].tenor
@@ -273,9 +308,14 @@ func membayar(p *data, idx int, bayar *float64) {
 		if (*p)[idx].totalPinjaman <= 0 {
 			(*p)[idx].totalPinjaman = 0
 			(*p)[idx].tenor = 0
+			*bayar = 0
 		}
-		fmt.Printf("Total Yang harus Dibayar adalah RP. %.2f Dengan tenor selama %d\n", bunga(*p, idx), (*p)[idx].tenor)
-		fmt.Printf("Total Yang harus dibayarkan bulan ini adalah %.2f\n", *bayar/float64((*p)[idx].tenor))
+		fmt.Printf("Total Yang harus Dibayar adalah RP. %.2f Dengan tenor selama %d bulan\n", bunga(*p, idx), (*p)[idx].tenor)
+		if (*p)[idx].totalPinjaman <= 0 {
+			fmt.Println("Pembayaran Sudah Lunas")
+		} else {
+			fmt.Printf("Total Yang harus dibayarkan bulan ini adalah Rp. %.2f\n", *bayar/float64((*p)[idx].tenor))
+		}
 		fmt.Println("Sudah Bayar ", bulan, " Bulan")
 		fmt.Println("Bayar Sekarang?")
 		fmt.Println("1. Ya")
@@ -313,93 +353,137 @@ func bunga(p data, idx int) float64 {
 	return float64(p[idx].totalPinjaman) + total
 }
 
-func menuAdm(p *data, idx int, bPengguna *int, pinjol *bool) {
+func menuAdm(p *data, ad *adm, idx int, bPengguna *int, pinjol *bool) {
+	clear()
 	var pilihan int
 
 	pilihan = -1
 	for pilihan != 0 {
 		fmt.Println("=== Menu Admin ===")
-		fmt.Println("1. Ubah Data")
-		fmt.Println("2. Hapus Data")
-		fmt.Println("3. Cari Data")
-		fmt.Println("4. Urutkan Pinjaman")
-		fmt.Println("5. Urutkan Tenor")
-		fmt.Println("0. Keluar")
-		fmt.Println("Pilih : ")
+		fmt.Println("1. Ubah Data Pengguna")
+		fmt.Println("2. Ubah Data Admin")
+		fmt.Println("3. Hapus Data")
+		fmt.Println("4. Cari Data")
+		fmt.Println("5. Urutkan Data Peminjaman")
+		fmt.Println("0. Kembali")
+		fmt.Print("Pilih : ")
 		fmt.Scan(&pilihan)
 		switch pilihan {
 		case 1:
 			ubah(&*p, &(*bPengguna))
 		case 2:
+			ubahAdm(&*ad, idx)
+		case 3:
 			hapus(&*p, &*bPengguna)
+		case 4:
+			caridata(*p, *bPengguna)
+		case 5:
+			Urut(p, *bPengguna)
 		}
 	}
 }
 
 func ubah(p *data, bPengguna *int) {
-	var idx, i, tes, tenor int
+	var idx, i, tes, tenor, id int
 	var nama string
-	tes = -1
-	idx = -1
-	for tes != 0 {
-		fmt.Println("Input Nama yang datanya ingin diubah")
-		fmt.Scan(&nama)
-		for i = 0; i < *bPengguna; i++ {
-			if nama == (*p)[i].nama {
-				idx = i
-			}
-		}
-		if idx == -1 {
-			fmt.Println("Data Tidak Ditemukan")
-		} else {
-			for tes != 0 {
-				fmt.Println("Data Ditemukan ")
-				fmt.Println("Nama : ", (*p)[idx].nama)
-				fmt.Println("Total Pinjaman : ", (*p)[idx].totalPinjaman)
-				fmt.Println("Tenor : ", (*p)[idx].tenor)
-				fmt.Println("Data mana yang ingin diubah")
-				fmt.Println("1.Nama")
-				fmt.Println("2.Password")
-				fmt.Println("3.Total Pinjaman")
-				fmt.Println("4.Tenor")
-				fmt.Println("0.Stop")
-				fmt.Scan(&tes)
-				switch tes {
-				case 1:
-					fmt.Print("Masukkan Nama Baru : ")
-					fmt.Scan(&(*p)[idx].nama)
-				case 2:
-					fmt.Print("Masukkan Password Baru : ")
-					fmt.Scan(&(*p)[idx].password)
-				case 3:
-					fmt.Print("Masukkan Pinjaman Baru : ")
-					fmt.Scan(&(*p)[idx].totalPinjaman)
-				case 4:
-					for tenor != 6 && tenor != 12 && tenor != 18 && tenor != 24 {
-						fmt.Println("Pilih Tenor Baru | 6 | 12 | 18 | 24 |")
-						fmt.Scan(&tenor)
-						if tenor == 6 || tenor == 12 || tenor == 18 || tenor == 24 {
-							(*p)[idx].tenor = tenor
-						}
-					}
-				case 0:
-					return
+	if *bPengguna >= 1 {
+		tes = -1
+		idx = -1
+		for tes != 0 {
+			fmt.Println(p[i].nama, p[i].id)
+			fmt.Println("Input Nama dan ID yang datanya ingin diubah")
+			fmt.Print("Input Nama : ")
+			fmt.Scan(&nama)
+			fmt.Print("Input Id :")
+			fmt.Scan(&id)
+
+			for i = 0; i < *bPengguna; i++ {
+				if nama == (*p)[i].nama && id == (*p)[i].id {
+					idx = i
 				}
 			}
+			if idx == -1 {
+				fmt.Println("Data Tidak Ditemukan")
+			} else {
+				for tes != 0 {
+					fmt.Println("Data Ditemukan ")
+					fmt.Println("Nama : ", (*p)[idx].nama)
+					fmt.Println("Total Pinjaman : ", (*p)[idx].totalPinjaman)
+					fmt.Println("Tenor : ", (*p)[idx].tenor)
+					fmt.Println("Data mana yang ingin diubah")
+					fmt.Println("1.Nama")
+					fmt.Println("2.Password")
+					fmt.Println("3.Total Pinjaman")
+					fmt.Println("4.Tenor")
+					fmt.Println("0.Stop")
+					fmt.Scan(&tes)
+					switch tes {
+					case 1:
+						fmt.Print("Masukkan Nama Baru : ")
+						fmt.Scan(&(*p)[idx].nama)
+					case 2:
+						fmt.Print("Masukkan Password Baru : ")
+						fmt.Scan(&(*p)[idx].password)
+					case 3:
+						fmt.Print("Masukkan Pinjaman Baru : ")
+						fmt.Scan(&(*p)[idx].totalPinjaman)
+					case 4:
+						for tenor != 6 && tenor != 12 && tenor != 18 && tenor != 24 {
+							fmt.Println("Pilih Tenor Baru | 6 | 12 | 18 | 24 |")
+							fmt.Scan(&tenor)
+							if tenor == 6 || tenor == 12 || tenor == 18 || tenor == 24 {
+								(*p)[idx].tenor = tenor
+							}
+						}
+					case 0:
+						return
+					}
+				}
+			}
+		}
+	} else {
+		fmt.Println("Belum ada Data Pengguna")
+	}
+}
+
+func ubahAdm(ad *adm, idx int) {
+	clear()
+	var pilih int
+	pilih = -1
+	for pilih != 0 {
+		fmt.Println("Halo", (*ad)[idx].nama, "Ingin Mengubah Data Apa?")
+		fmt.Println("1.Nama")
+		fmt.Println("2.Password")
+		fmt.Println("0.Keluar")
+		fmt.Print("Pilihan : ")
+		fmt.Scan(&pilih)
+		switch pilih {
+		case 1:
+			fmt.Println("Nama Baru :")
+			fmt.Scan(&(*ad)[idx].nama)
+		case 2:
+			fmt.Println("Password Baru :")
+			fmt.Scan(&(*ad)[idx].password)
+		default:
+			fmt.Println("Input Tidak Sesuai")
 		}
 	}
 }
 
 func hapus(p *data, bPengguna *int) {
+	clear()
 	var nama string
-	var idx, i int
+	var idx, i, id int
 	idx = -1
 
-	fmt.Print("Masukkan nama pengguna yang ingin dihapus: ")
+	fmt.Println("Masukkan Nama dan Id pengguna yang ingin dihapus")
+	fmt.Print("Masukkan Nama : ")
 	fmt.Scan(&nama)
+	fmt.Print("Masukkan ID : ")
+	fmt.Scan(&id)
 
 	for i = 0; i < *bPengguna; i++ {
-		if (*p)[i].nama == nama && idx == -1 {
+		if (*p)[i].nama == nama && (*p)[i].id == id && idx == -1 {
 			idx = i
 		}
 	}
@@ -415,4 +499,145 @@ func hapus(p *data, bPengguna *int) {
 
 	*bPengguna--
 	fmt.Println("Data berhasil dihapus.")
+}
+func carid(p data, bPengguna int, x string, idx *int) pengguna {
+	var left, right, mid int
+	*idx = -1
+	left = 0
+	right = bPengguna - 1
+	for left <= right && *idx == -1 {
+		mid = (left + right) / 2
+		if x < p[mid].nama {
+			right = mid - 1
+		} else if x > p[mid].nama {
+			left = mid + 1
+		} else {
+			*idx = mid
+		}
+
+	}
+	return p[*idx]
+}
+func caridata(p data, bPengguna int) {
+	var x string
+	cetaktotal(p, bPengguna)
+	fmt.Print("Ketikan nama pengguna yang ingin anda cari: ")
+	fmt.Scan(&x)
+	fmt.Println(" ")
+	var cek int
+	carid(p, bPengguna, x, &cek)
+	cetakcari(p, bPengguna, cek)
+}
+func cetakcari(p data, bPengguna, cek int) {
+	var status string
+	fmt.Printf("%-20s %-20s %-15s %-15s\n", "Nama", "Total Peminjaman", "Tenor(bulan)", "Status")
+	for i := 0; i < bPengguna; i++ {
+		if p[i].status == false {
+			status = "Belum Lunas"
+		} else {
+			status = "Sudah Lunas"
+		}
+		if p[i] == p[cek] {
+			fmt.Printf("%-20s %-20.2f %-15d %-15s\n", p[i].nama, p[i].totalPinjaman, p[i].tenor, status)
+		}
+	}
+}
+func cetaktotal(p data, bPengguna int) {
+	var status string
+	fmt.Printf("%-20s %-20s %-15s %-15s\n", "Nama", "Total Peminjaman", "Tenor(bulan)", "Status")
+	for i := 0; i < bPengguna; i++ {
+		if p[i].status == false {
+			status = "Belum Lunas"
+		} else {
+			status = "Sudah Lunas"
+		}
+		fmt.Printf("%-20s %-20.2f %-15d %-15s\n", p[i].nama, p[i].totalPinjaman, p[i].tenor, status)
+	}
+	fmt.Println(" ")
+}
+func Urut(p *data, bPengguna int) {
+	var pilih int
+	cetaktotal(*p, bPengguna)
+	fmt.Println("1. Urutkan Berdasarkan Jumlah Peminjaman")
+	fmt.Println("2. Urutkan Berdasarkan Jumlah Tenor")
+	fmt.Print("Data yang ingin diurutkan : ")
+	fmt.Scan(&pilih)
+	switch pilih {
+	case 1:
+		urutpinjam(p, bPengguna)
+	case 2:
+		uruttenor(p, bPengguna)
+	}
+
+}
+func urutpinjam(p *data, bPengguna int) {
+	var pass, pil int
+	var temp pengguna
+	pass = 1
+	fmt.Print(" (1) untuk menaik, (2) untuk menurun : ")
+	fmt.Scan(&pil)
+	if pil == 1 {
+		for pass < bPengguna {
+			i := pass
+			temp = p[pass]
+			for i > 0 && temp.totalPinjaman < p[i-1].totalPinjaman {
+				p[i] = p[i-1]
+				i--
+			}
+			p[i] = temp
+			pass++
+		}
+	} else if pil == 2 {
+		for pass < bPengguna {
+			i := pass
+			temp = p[pass]
+			for i > 0 && temp.totalPinjaman > p[i-1].totalPinjaman {
+				p[i] = p[i-1]
+				i--
+			}
+			p[i] = temp
+			pass++
+		}
+	} else {
+		fmt.Println("Invalid")
+	}
+	cetaktotal(*p, bPengguna)
+}
+func uruttenor(p *data, bPengguna int) {
+	var pass, pil int
+	var temp pengguna
+	pass = 1
+	fmt.Print(" (1) untuk menaik, (2) untuk menurun : ")
+	fmt.Scan(&pil)
+	if pil == 1 {
+		for pass < bPengguna {
+			i := pass
+			temp = p[pass]
+			for i > 0 && temp.tenor < p[i-1].tenor {
+				p[i] = p[i-1]
+				i--
+			}
+			p[i] = temp
+			pass++
+		}
+	} else if pil == 2 {
+		for pass < bPengguna {
+			i := pass
+			temp = p[pass]
+			for i > 0 && temp.tenor > p[i-1].tenor {
+				p[i] = p[i-1]
+				i--
+			}
+			p[i] = temp
+			pass++
+		}
+	} else {
+		fmt.Println("Invalid")
+	}
+	cetaktotal(*p, bPengguna)
+}
+func clear() {
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
